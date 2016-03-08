f90df60061b31c347e423712664e486f16a5229b
dclark87 <danieljclark87@gmail.com>
2016-03-07 22:45:55 -0500

    Got CUnit working for unit testing

--- test/unit/string_arrays_test.c
+++ test/unit/string_arrays_test.c
@@ -7,40 +7,98 @@
 #include <stdio.h>
 #include <string.h>
 
-#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
 
 #include "../../include/strings_arrays.h"
 
 // Test the reverse function
-int test_reverse()
+void test_reverse(void)
 {
     char test_str1[] = "abcd";
     char test_str1_rev[] = "dcba";
     reverse(test_str1);
-    printf("test_str1: %s\n", test_str1);
-    printf("test_str1_rev: %s\n", test_str1_rev);
-    int ret = strcmp(test_str1, test_str1_rev);
-    printf("retval for strcmp is: %d\n", ret);
-    CU_ASSERT_EQUAL(test_str1, test_str1_rev);
-    return(1);
+    CU_ASSERT_STRING_EQUAL(test_str1, test_str1_rev);
 }
 
-int maxi(int i1, int i2)
+/* Pointer to the file used by the tests. */
+static FILE* temp_file = NULL;
+
+/* The suite initialization function.
+ * Opens the temporary file used by the tests.
+ * Returns zero on success, non-zero otherwise.
+ */
+int init_suite1(void)
 {
-  return (i1 > i2) ? i1 : i2;
+   if (NULL == (temp_file = fopen("temp.txt", "w+"))) {
+      return -1;
+   }
+   else {
+      return 0;
+   }
 }
 
-void test_maxi(void)
+/* The suite cleanup function.
+ * Closes the temporary file used by the tests.
+ * Returns zero on success, non-zero otherwise.
+ */
+int clean_suite1(void)
 {
-  CU_ASSERT(maxi(0,2) == 2);
-  CU_ASSERT(maxi(0,-2) == 0);
-  CU_ASSERT(maxi(2,2) == 2);
+   if (0 != fclose(temp_file)) {
+      return -1;
+   }
+   else {
+      temp_file = NULL;
+      return 0;
+   }
 }
 
-// Main function
+/* Simple test of fprintf().
+ * Writes test data to the temporary file and checks
+ * whether the expected number of bytes were written.
+ */
+void testFPRINTF(void)
+{
+   int i1 = 10;
+
+   if (NULL != temp_file) {
+      CU_ASSERT(0 == fprintf(temp_file, ""));
+      CU_ASSERT(2 == fprintf(temp_file, "Q\n"));
+      CU_ASSERT(7 == fprintf(temp_file, "i1 = %d", i1));
+   }
+}
+
+
+/* The main() function for setting up and running the tests.
+ * Returns a CUE_SUCCESS on successful running, another
+ * CUnit error code on failure.
+ */
 int main()
 {
-    //int pass = test_reverse();
-    test_maxi();
-    return(0);
+   CU_pSuite pSuite = NULL;
+
+   /* initialize the CUnit test registry */
+   if (CUE_SUCCESS != CU_initialize_registry())
+      return CU_get_error();
+
+   /* add a suite to the registry */
+   pSuite = CU_add_suite("Suite_1", init_suite1, clean_suite1);
+   if (NULL == pSuite) {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* add the tests to the suite */
+   /* NOTE - ORDER IS IMPORTANT - MUST TEST fread() AFTER fprintf() */
+   if ((NULL == CU_add_test(pSuite, "test of fprintf()", testFPRINTF)) ||
+       (NULL == CU_add_test(pSuite, "test of reverse()", test_reverse)))
+   {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* Run all tests using the CUnit Basic interface */
+   CU_basic_set_mode(CU_BRM_VERBOSE);
+   CU_basic_run_tests();
+   CU_cleanup_registry();
+   return CU_get_error();
 }
